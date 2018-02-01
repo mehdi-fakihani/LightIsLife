15050b128357b2455ac4f9f365dc0315285851d3
Sidney Congard <sidney.congard@gmail.com>
2018-02-01 11:01:09 -0500

    Revert "cleaned branch"
    This reverts commit fd06bff948f01560a4e238b845edf25c24c69f84.

---
+++ Assets/Scenes/Sidney/HeroController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LIL.Inputs;
+using UnityEngine;
+
+namespace LIL
+{
+    public class HeroController : MonoBehaviour
+    {
+        private Profile profile;
+        private Skill fireball;
+        
+        void Start()
+        {
+            profile = new Profile(ProfilsID.XBoxGamepad, 0);
+            var skillManager = GetComponent<SkillManager>();
+            fireball = skillManager.addSkill("fireball");
+        }
+        
+        void Update()
+        {
+            var trans = gameObject.transform;
+            
+            if (profile.getKeyDown(PlayerAction.Attack))
+            {
+                bool success = fireball.tryCast();
+                Debug.Log("cast success ? " + success);
+            }
+        }
+    }
+}
---
+++ Assets/Scenes/Sidney/MissileModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LIL.Skills
+{
+    public class MissileModel : ISkillModel
+    {
+        [SerializeField] public float castTime;
+        [SerializeField] public float speed;
+        [SerializeField] public GameObject missile;
+        
+        public override void cast(SkillManager manager)
+        {
+            var effects = manager.GetComponent<EffectManager>();
+            effects.addEffect(new Effects.Silence(castTime));
+            effects.addEffect(new Effects.Delayed(castTime, () =>
+            {
+                var caster = manager.gameObject.transform;
+                var missilePos = caster.position + caster.forward * 2;
+                var obj = Instantiate(
+                    missile,
+                    missilePos,
+                    caster.rotation);
+
+                var body = obj.GetComponent<Rigidbody>();
+                body.velocity = caster.forward * speed;
+            }));
+        }
+    }
+}
