aa2500001391442c645d53ef20d7b2141b9382f2
JulienQC <julien.cusset33@gmail.com>
2018-02-05 18:19:30 -0500

    Scene synchro: déplacement du joueur et du squelette sur le terrain, placement automatique de la caméra, animations de marche et d'attaque.

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;               // Reference to the player's position.
+    NavMeshAgent nav;               // Reference to the nav mesh agent.
+
+
+    void Awake()
+    {
+        // Set up the references.
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        nav.SetDestination(player.position);
+    }
+}
