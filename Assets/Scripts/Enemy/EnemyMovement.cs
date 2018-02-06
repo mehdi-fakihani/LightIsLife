69012ce94d047d1ffb0a4563200af30738704687
JulienQC <julien.cusset33@gmail.com>
2018-02-06 10:55:18 -0500

    correction bug déplacement du joueur par le squeulette

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -4,20 +4,35 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    Transform player;               // Reference to the player's position.
-    NavMeshAgent nav;               // Reference to the nav mesh agent.
+    Transform player;               
+    NavMeshAgent nav;              
+    Animator anim;
 
+    bool moveCancelled = false;
 
     void Awake()
     {
         // Set up the references.
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        anim = GetComponent<Animator>();
         nav = GetComponent<NavMeshAgent>();
     }
 
 
     void Update()
     {
-        nav.SetDestination(player.position);
+        if (anim.GetBool("Move"))
+        {
+            nav.SetDestination(player.position);
+            moveCancelled = false;
+        }
+        else
+        {
+            if (!moveCancelled)
+            {
+                nav.SetDestination(transform.position);
+                moveCancelled = true;
+            }
+        }
     }
 }
