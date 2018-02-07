5655461057cc84a36d8352ce333925b81e16156f
Dorian Boché <dorian.boche@outlook.fr>
2018-02-07 13:13:03 -0500

    add deplacement

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -2,7 +2,7 @@
 
 public class EnemyManager : MonoBehaviour
 {
-    public PlayerHealth playerHealth;       // Reference to the player's heatlh.
+    //public PlayerHealth playerHealth;       // Reference to the player's heatlh.
     public GameObject enemy;                // The enemy prefab to be spawned.
     public float spawnTime = 3f;            // How long between each spawn.
     public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
@@ -18,11 +18,11 @@ public class EnemyManager : MonoBehaviour
     void Spawn()
     {
         // If the player has no health left...
-        if (playerHealth.currentHealth <= 0f)
+        /*if (playerHealth.currentHealth <= 0f)
         {
             // ... exit the function.
             return;
-        }
+        }*/
 
         // Find a random index between zero and one less than the number of spawn points.
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
