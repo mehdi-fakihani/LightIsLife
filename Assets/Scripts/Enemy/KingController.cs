524033fa50c5009a06c8b12d1740e401d8c32afe
JulienQC <julien.cusset33@gmail.com>
2018-03-24 10:52:01 -0400

    boss aoe and invocations

--- Assets/Scripts/Enemy/KingController.cs
+++ Assets/Scripts/Enemy/KingController.cs
@@ -1,16 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
-public class KingController : MonoBehaviour {
+namespace LIL
+{
+    public class KingController : MonoBehaviour
+    {
+        
+        public GameObject poisonPrefab;
+        public float poisonAoeRange = 10f;
+        public float poisonDamage = 10f;
+        public float poisonActivationTime = 1.5f;
+        public float poisonFrequence = 2f;           // make poison spawn every x seconds
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        public GameObject invocation;
+        public float invocationRange = 2f;
+        public int nbInvocations = 4;
+
+
+        private void ActivatePoisonAoe()
+        {
+            InvokeRepeating("SpawnPoisonAoe", 0, poisonFrequence);
+        }
+
+        private void InvocationSpell()
+        {
+            for (int i = 0; i < nbInvocations; i++)
+            {
+                Instantiate(invocation, GetRandomPosInRange(invocationRange), Quaternion.Euler(0, 0, 0));
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            ActivatePoisonAoe();
+            InvocationSpell();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        private void SpawnPoisonAoe()
+        {
+            KingPoisonController.CreatePoisonAoe(poisonPrefab, GetRandomPosInRange(poisonAoeRange),
+                                    Quaternion.Euler(0, 0, 0), poisonDamage, poisonActivationTime);
+        }
+
+        private Vector3 GetRandomPosInRange(float range)
+        {
+            float dx = Random.Range(-range, range + 1);
+            float dz = Random.Range(-range, range + 1);
+            Vector3 position = new Vector3(transform.position.x + dx,
+                                           transform.position.y + 0.5f,
+                                           transform.position.z + dz);
+            return position;
+        }
+    }
 }
+
---
+++ Assets/Scripts/Enemy/KingPoisonController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LIL
+{
+    public class KingPoisonController : MonoBehaviour
+    {
+        public float Damage { get;  set; }
+        public float DamageActivationTime { get; set; }
+
+        private Collider aoe;
+
+        public static void CreatePoisonAoe(Object prefab, Vector3 pos, Quaternion rot, float damage, float activationTime)
+        {
+            GameObject obj = Instantiate(prefab, pos, rot) as GameObject;
+            KingPoisonController kpc = obj.GetComponent<KingPoisonController>();
+            kpc.Damage = damage;
+            kpc.DamageActivationTime = activationTime;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                other.gameObject.GetComponent<HealthManager>().harm(Damage);
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            aoe = GetComponent<Collider>();
+            aoe.enabled = false;
+            StartCoroutine(PoisonLifeCycle());
+        }
+
+        private IEnumerator PoisonLifeCycle()
+        {
+            // let the player see the poison before activation so he can dodge it
+            transform.GetChild(0).gameObject.SetActive(true);
+            yield return new WaitForSeconds(DamageActivationTime);
+
+            // Activate the damage zone for 1 second
+            aoe.enabled = true;
+            transform.GetChild(1).gameObject.SetActive(true);
+            yield return new WaitForSeconds(1);
+
+            // Then destroy poison zone
+            Destroy(gameObject);
+        }
+
+    }
+}
+
