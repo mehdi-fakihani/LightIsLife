2c2a191570e4acadefcd93b83d67241d807f0ed4
Sidney Congard <sidney.congard@gmail.com>
2018-03-27 17:11:03 -0400

    added Whirlwind spell, not tested yet

---
+++ Assets/Skills/Whirlwind/WhirlwindEffect.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LIL.Effects
+{
+
+    public class Whirlwind : IEffect
+    {
+        private readonly WhirlwindSkill skill;
+        private readonly float impactDamages;
+        private float currentTime;
+        private GameObject castEffect;
+
+        public Whirlwind(WhirlwindSkill skill)
+        {
+            this.skill = skill;
+            float occurences = skill.duration / skill.impactPeriod;
+            impactDamages = skill.maxDamages / occurences;
+        }
+
+        protected override float duration()
+        {
+            return skill.duration;
+        }
+
+        protected override void apply()
+        {
+            currentTime = 0f;
+            castEffect = Object.Instantiate(
+                skill.impactEffect,
+                manager.transform.position,
+                skill.impactEffect.transform.rotation);
+        }
+
+        protected override void update(float secs)
+        {
+            currentTime += secs;
+            castEffect.transform.position = manager.transform.position;
+
+            manager.transform.Rotate(Vector3.up, secs * skill.speedRotation);
+
+            if (currentTime < skill.impactPeriod) return;
+            currentTime -= skill.impactPeriod;
+
+            var hits = Physics.OverlapSphere(manager.transform.position, skill.range);
+            foreach (var hit in hits)
+            {
+                if (!manager.gameObject.isEnemyWith(hit.gameObject)) continue;
+                var health = hit.GetComponent<HealthManager>();
+                if (!health) continue;
+                health.harm(impactDamages);
+                var impact = Object.Instantiate(skill.impactEffect, hit.transform.position, Quaternion.identity);
+                Object.Destroy(impact, 1f);
+            }
+        }
+
+        public override void expire(bool onDeath)
+        {
+            Object.Destroy(castEffect);
+        }
+    }
+
+}
---
+++ Assets/Skills/Whirlwind/WhirlwindSkill.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LIL
+{
+
+    public class WhirlwindSkill : ISkillModel
+    {
+        public float maxDamages;
+        public float range;
+        public float duration;
+        public float slowRatio;
+        public float impactPeriod;
+        public float speedRotation;
+        public GameObject castEffect;
+        public GameObject impactEffect;
+        public AudioClip castSound;
+        
+        public override void cast(SkillManager manager)
+        {
+            var caster = manager.gameObject;
+
+            if (castSound != null)
+            {
+                var audioSource = caster.GetComponent<AudioSource>();
+                audioSource.PlayOneShot(castSound, 0.3f);
+            }
+
+            var cast = Instantiate(castEffect, caster.transform.position, Quaternion.identity);
+            Destroy(cast, duration);
+            
+            caster.GetComponent<EffectManager>().addEffect(new Effects.Slow(duration, slowRatio));
+            caster.GetComponent<EffectManager>().addEffect(new Effects.Silence(duration));
+            caster.GetComponent<EffectManager>().addEffect(new Effects.Whirlwind(this));
+        }
+    }
+
+}
