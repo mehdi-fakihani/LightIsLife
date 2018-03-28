4b94ce542df937fc28f5b34460fd949de119d07d
Sidney Congard <sidney.congard@gmail.com>
2018-03-28 11:31:38 -0400

    added effect on charge impact

---
+++ Assets/Skills/Poison/PoisonEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LIL.Effects
+{
+
+    public class Poison : IEffect
+    {
+        protected override float duration()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void apply()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void update(float secs)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void expire(bool onDeath)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
