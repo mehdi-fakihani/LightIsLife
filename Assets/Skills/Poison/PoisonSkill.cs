5c98eacd2c6d73ebc664223a8f2a731b21378692
Sidney Congard <sidney.congard@gmail.com>
2018-03-29 10:02:58 -0400

    finished poison skill

--- Assets/Skills/Poison/PoisonSkill.cs
+++ Assets/Skills/Poison/PoisonSkill.cs
@@ -9,13 +9,28 @@ namespace LIL
     {
         [SerializeField] private float totalDamages;
         [SerializeField] private float effectDuration;
+        [SerializeField] private float buffDuration;
         [SerializeField] private GameObject impactEffect;
         [SerializeField] private GameObject castEffect;
         [SerializeField] private AudioClip castSound;
 
         public override void cast(SkillManager manager)
         {
+            var caster = manager.gameObject;
 
+            if (castSound != null)
+            {
+                var audioSource = caster.GetComponent<AudioSource>();
+                audioSource.PlayOneShot(castSound);
+            }
+
+            var effect = Instantiate(castEffect, manager.transform.position, Quaternion.identity);
+            Destroy(effect, 2f);
+            
+            caster.GetComponent<EffectManager>().addEffect(
+                new Effects.PoisonBuff(buffDuration, effectDuration, () =>
+                new Effects.Poison(effectDuration, totalDamages, impactEffect)
+            ));
         }
     }
 
